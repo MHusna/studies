36287299e0184e93600b07a27ca3e1826e95adda
MHusna-Kurs <mehmethusnakurs@gmail.com>
2022-12-26 08:56:50 +0300

    sliding-tabs örnek projesi düzenlendi.

---
+++ 05-sliding-tabs/script.js
@@ -0,0 +1,19 @@
+function movement(number){
+
+    var slider_bg = document.getElementsByClassName("slider-bg")[0];
+    var clicked = document.getElementsByTagName("a")[number];
+
+    for(var i=0;i<3;i++){
+		var a = document.getElementsByTagName("a")[i]; 
+		a.style.color="black";
+	}
+
+    slider_bg.style.position="relative";
+    slider_bg.style.left=clicked.offsetLeft+"px";
+    slider_bg.style.transition = "0.5s";
+    slider_bg.style.zIndex="1";
+
+    clicked.style.zIndex="2";
+	clicked.style.color="white";
+	clicked.style.transition="color 1s";
+}
